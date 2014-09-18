078e488bc793570400b1e22a08168eff5fc3d381
Xavier Defrang <xavier.defrang@gmail.com>
2014-09-18 19:33:01 +0200

    First shot at exercises for meetup 2

---
+++ meetup2/completed_exercises/counter.exs
@@ -0,0 +1,125 @@
+#
+# Let's extend our little counter process example!
+#
+# Your tasks:
+#  1) Add a new Counter.dec/1 public function which decrements the counter, make sure your write a test case for it
+#  2) A counter must always remain >= 0, make sure an exception is raised otherwise.
+#
+#     You can raise an exception with:
+#
+#       raise ArgumentError
+#
+#     Testing exceptions is straightforward:
+#
+#        assert_raise SomeException, fn ->
+#          ... some code supposed to raise SomeException ...
+#        end
+#
+
+defmodule Counter do
+
+  @doc "A simple counter showing how to manage state using processes"
+
+  @doc "Creates a new counter, initialized to 0"
+  def new do
+    spawn_link(fn -> loop(0) end)
+  end
+
+  @doc "Increments the given counter by 1"
+  def inc(counter) do
+    send counter, {self, :inc}
+  end
+
+  @doc "Decrements the given counter by 1"
+  def dec(counter) do
+    send counter, {self, :dec}
+  end
+
+  @doc "Returns the current value of the given counter"
+  def value(counter) do
+    send counter, {self, :val}
+    get_value counter
+  end
+
+  @doc "Destroy the given counter and return its last value"
+  def terminate(counter) do
+    send counter, {self, :bye}
+    get_value counter
+  end
+
+  defp get_value(counter) do
+    receive do
+      {^counter, :ok, val} ->
+        val
+    end
+  end
+
+  defp loop(counter) do
+    receive do
+      {_, :inc} ->
+        loop(counter + 1)
+      {_, :dec} ->
+        loop(safe_decrement(counter))
+      {sender, :val} ->
+        send sender, {self, :ok, counter}
+        loop(counter)
+      {sender, :bye} ->
+        send sender, {self, :ok, counter}
+    end
+  end
+
+  defp safe_decrement(0),       do: raise(ArgumentError, "Counter cannot get negative")
+  defp safe_decrement(counter), do: counter - 1
+
+end
+
+ExUnit.start
+
+defmodule CounterTest do
+  use ExUnit.Case
+
+  test "counter starts at 0" do
+    c = Counter.new
+    assert 0 == Counter.value(c)
+  end
+
+  test "counter incrementation" do
+    c = Counter.new
+    Counter.inc(c)
+    Counter.inc(c)
+    Counter.inc(c)
+    assert 3 == Counter.value(c)
+  end
+
+  test "counter decrementation" do
+    c = Counter.new
+    Counter.inc(c)
+    Counter.dec(c)
+    Counter.inc(c)
+    assert 1 == Counter.value(c)
+  end
+
+  test "counter decrementation below zero" do
+    c = Counter.new
+    assert_raise ArgumentError, fn -> Counter.dec(c) end
+  end
+
+  test "multiple counters" do
+    c1 = Counter.new
+    c2 = Counter.new
+    c3 = Counter.new
+    Counter.inc(c1)
+    Counter.inc(c2)
+    Counter.inc(c2)
+    assert 1 == Counter.value(c1)
+    assert 2 == Counter.value(c2)
+    assert 0 == Counter.value(c3)
+  end
+
+  test "terminate returns final value of counter" do
+    c = Counter.new
+    Counter.inc(c)
+    assert 1 == Counter.terminate(c)
+  end
+
+end
---
+++ meetup2/completed_exercises/parallel_map.exs
@@ -0,0 +1,47 @@
+defmodule Parallel do
+
+  def map(enumerable, func) do
+    enumerable
+      |> spawn_mapping_processes(func)
+      |> collect_results
+  end
+
+  defp spawn_mapping_processes(enumerable, func) do
+    parent_pid = self
+    Enum.map enumerable, fn (item) ->
+      spawn_link(fn ->
+        send parent_pid, {self, func.(item)}
+      end)
+    end
+  end
+
+  defp collect_results(pids) do
+    Enum.map pids, fn (pid) ->
+      receive do
+        {^pid, mapped_item} -> mapped_item
+      end
+    end
+  end
+
+end
+
+
+
+ExUnit.start
+
+defmodule ParallelTest do
+   use ExUnit.Case
+
+  test "it maps empty enumerables" do
+    assert [] == Parallel.map([], fn (x) -> x*x end)
+  end
+
+  test "it maps enumerables" do
+    assert [0, 1, 4, 9, 16] == Parallel.map([0, 1, 2, 3, 4], fn (x) -> x*x end)
+  end
+
+  test "it maps large enumerables" do
+    assert [0, 1, 4, 9, 16|_] = Parallel.map(0..99999, fn (x) -> x*x end)
+  end
+
+end
